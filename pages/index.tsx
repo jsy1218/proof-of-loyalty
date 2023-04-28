d8309e2a46cd44f546fcde0fb289c99922ff6690
Siyu Jiang <blackjackjiang@gmail.com>
2023-04-27 20:16:57 -0700

    added all the blue-chips collections

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,12 +18,12 @@ const Index = () => {
 
 								<div className="col-xl-12">
 									<h4>Pre-selected collections</h4>
-									<p>Loads faster if collections of the same brand (e.g. Azuki) are chosen together</p>
+									<p>Loads faster if all collections of the same brand (e.g. Azuki) are chosen together</p>
 									<div className="intro-search">
 										<form action="#">
 											<Multiselect
-												displayValue="key"
-												groupBy="collection"
+												displayValue="collection"
+												groupBy="brand"
 												placeholder="Click on any collections"
 												onKeyPressFn={function noRefCheck(){}}
 												onRemove={function noRefCheck(){}}
@@ -31,33 +31,100 @@ const Index = () => {
 												onSelect={function noRefCheck(){}}
 												options={[
 													{
-														collection: 'Azuki',
-														key: 'PFP'
+														brand: 'Azuki',
+														collection: 'PFP',
+														address: '0xed5af388653567af2f388e6224dc7c4b3241c544'
 													},
 													{
-														collection: 'Azuki',
-														key: 'BEANZ'
+														brand: 'Azuki',
+														collection: 'BEANZ',
+														address: '0x306b1ea3ecdf94ab739f1910bbda052ed4a9f949'
 													},
 													{
-														cat: 'Group 1',
-														key: 'Option 3'
+														brand: 'Doodles LLC',
+														collection: 'Doodles',
+														address: '0x8a90cab2b38dba80c64b7734e58ee1db38b8992e'
 													},
 													{
-													cat: 'Group 2',
-													key: 'Option 4'
+														brand: 'Doodles LLC',
+														collection: 'Space Doodles',
+														address: '0x620b70123fb810f6c653da7644b5dd0b6312e4d8'
 													},
 													{
-													cat: 'Group 2',
-													key: 'Option 5'
+														brand: 'Doodles LLC',
+														collection: 'Dooplicators',
+														address: '0x466cfcd0525189b573e794f554b8a751279213ac'
 													},
 													{
-													cat: 'Group 2',
-													key: 'Option 6'
+														brand: 'Doodles LLC',
+														collection: 'Genesis Box',
+														address: '0xb75f09b4340aeb85cd5f2dd87d31751edc11ed39'
 													},
 													{
-													cat: 'Group 2',
-													key: 'Option 7'
-													}
+														brand: 'Memeland',
+														collection: 'YOU THE REAL MVP',
+														address: '0x6efc003d3f3658383f06185503340c2cf27a57b6'
+													},
+													{
+														brand: 'Memeland',
+														collection: 'The Captainz',
+														address: '0x769272677fab02575e84945f03eca517acc544cc'
+													},
+													{
+														brand: 'Memeland',
+														collection: 'The Potatoz',
+														address: '0x39ee2c7b3cb80254225884ca001f57118c8f21b6'
+													},
+													{
+														brand: 'Proof',
+														collection: 'Proof Collective',
+														address: '0x08d7c0242953446436f34b4c78fe9da38c73668d'
+													},
+													{
+														brand: 'Proof',
+														collection: 'Moonbirds',
+														address: '0x23581767a106ae21c074b2276d25e5c3e136a68b'
+													},
+													{
+														brand: 'Proof',
+														collection: 'Oddities',
+														address: '0x1792a96e5668ad7c167ab804a100ce42395ce54d'
+													},
+													{
+														brand: 'RTFKT',
+														collection: 'Clone X',
+														address: '0x49cf6f5d44e70224e2e23fdcdd2c053f30ada28b'
+													},
+													{
+														brand: 'Yugalabs',
+														collection: 'Bored Ape Yacht Club',
+														address: '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d'
+													},
+													{
+														brand: 'Yugalabs',
+														collection: 'Mutant Ape Yacht Club',
+														address: '0x60e4d786628fea6478f785a6d7e704777c86a7c6'
+													},
+													{
+														brand: 'Yugalabs',
+														collection: 'Bored Ape Kennel Club',
+														address: '0xba30e5f9bb24caa003e9f2f0497ad287fdf95623'
+													},
+													{
+														brand: 'Yugalabs',
+														collection: 'CryptoPunks',
+														address: '0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb'
+													},
+													{
+														brand: 'Yugalabs',
+														collection: 'Meebits',
+														address: '0x7bd29408f11d2bfc23c34f18275bbf23bb716bc7'
+													},
+													{
+														brand: 'Yugalabs',
+														collection: 'Otherdeed for Otherside',
+														address: '0x34d85c9cdeb23fa97cb08333b511ac86e1c4e258'
+													},
 												]}
 												showCheckbox
 											/>
