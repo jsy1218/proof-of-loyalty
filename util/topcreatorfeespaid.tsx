bc124e04780bf69cd5e4ef66eac75ad4ddd1179e
Siyu Jiang <blackjackjiang@gmail.com>
2023-06-23 22:32:04 -0700

    refactor the query for other leaderboard

--- util/topcreatorfeespaid.tsx
+++ util/topcreatorfeespaid.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import Leaderboard from "../pages/leaderboard";
 import TableHeaderProps from "./tableheaderprops";
 import TableRowsProps from "./tablerowprops";
+import { flipsideQueryResult } from "./singleflipsidequery";
 
 const topCreatorFeesPaid = async (addresses: Array<string>) => {
     const myHeaders = new Headers();
@@ -108,122 +109,7 @@ const topCreatorFeesPaid = async (addresses: Array<string>) => {
 
     select TOP ${pageSize} * from output`;
 
-    var raw = JSON.stringify({
-      "jsonrpc": "2.0",
-      "method": "createQueryRun",
-      "params": [
-        {
-          "resultTTLHours": 1,
-          "maxAgeMinutes": 0,
-          "sql": sql,
-          "tags": {
-            "source": "proof-of-loyalty",
-            "env": "prod"
-          },
-          "dataSource": "snowflake-default",
-          "dataProvider": "flipside"
-        }
-      ],
-      "id": 1
-    });
-
-    var requestOptions: RequestInit = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow'
-    };
-
-    const response = await fetch("https://api-v2.flipsidecrypto.xyz/json-rpc", requestOptions)
-      .then(response => { 
-        return response.text();
-      } 
-    );
-    const queryRunId = JSON.parse(response).result.queryRun.id
-
-    var raw = JSON.stringify({
-      "jsonrpc": "2.0",
-      "method": "getQueryRun",
-      "params": [
-        {
-          "queryRunId": queryRunId
-        }
-      ],
-      "id": 1
-    });
-    
-    var requestOptions: RequestInit = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow'
-    };
-    
-    var queryState = 'UNDEFINED'
-    do {
-      try {
-        const queryRun = await fetch("https://api-v2.flipsidecrypto.xyz/json-rpc", requestOptions)
-          .then(response => {
-            return response.text();
-          } 
-        );
-        queryState = JSON.parse(queryRun).result.queryRun.state
-      } catch (error) {
-        console.log(error)
-      }
-    } while (queryState !== "QUERY_STATE_SUCCESS")
-          
-    var raw = JSON.stringify({
-      "jsonrpc": "2.0",
-      "method": "getQueryRunResults",
-      "params": [
-        {
-          "queryRunId": queryRunId,
-          "format": "csv",
-          "page": {
-            "number": 1,
-            "size": pageSize
-          }
-        }
-      ],
-      "id": 1
-    });
-    
-    var requestOptions: RequestInit = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow'
-    };
-    
-    const queryResult = await fetch("https://api-v2.flipsidecrypto.xyz/json-rpc", requestOptions)
-      .then(response => {
-        return response.text(); 
-      } 
-    );
-    const resultJson = JSON.parse(queryResult)
-    console.log(resultJson)
-    console.log(resultJson.result.rows)
-    const rows: Array<Row> = resultJson.result.rows
-    const columns: Array<string> = resultJson.result.columnNames
-    const columnTypes: Array<string> = resultJson.result.columnTypes
-
-    console.log(rows)
-
-    const records: Array<QueryResultRecord> = rows.map((row) => 
-      { 
-        let record: QueryResultRecord = {};
-        row.map((item, itemIndex) => 
-        { 
-          record[columns[itemIndex].toLowerCase()] = item
-        }) 
-        return record
-      }
-    )
-
-    const result: QueryResultSet = { queryId: queryRunId, status: "finished", columns: columns, columnTypes: columnTypes, rows: rows, runStats: null, records: records, error: null }
-
-    return result;
+    return await flipsideQueryResult(sql, pageSize)
 }
 
 // TODO improve ugly way to keep track of the UI state and react to new addresses input
